989c7b3065b3f0bf42282576ec44977bdb1ea63c
Egamberdi Ibrohimov <fullstackmernwebdeveloper@gmail.com>
2023-05-12 14:41:46 +0500

    API Updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+	baseURL: "https://mern-memories-api-part2.onrender.com",
+});
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem("profile")) {
 		req.headers.Authorization = `Bearer ${
